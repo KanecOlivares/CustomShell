9437ff65fe756eb74765bd2ce0a9a9e2ba9be701
Kanec Olivares <kanecolivares@gmail.com>
2025-07-17 12:41:10 -0700

    Added loading executable to foreground.

--- main.c
+++ main.c
@@ -18,19 +18,7 @@
 #define MAX_STATE_LEN 11
 #define DEBUG 1
 
-struct Job {
-    int id;
-    int pid;
-    int gpid;
-    struct Job* jobs[MAX_JOBS];
-    char state[MAX_STATE_LEN];
-};
-
-struct JobScheduler{
-    
-    int gpid;
-
-};
+volatile sig_atomic_t foreground_pid = -1;
 
 void throw_error(const char func_name[MAX_FUNCNAME_LEN], const char msg[MAX_MSG_LEN]){
     printf("Error in: %s:\n\t\t%s", func_name, msg);
@@ -40,6 +28,43 @@ void debug_msg(const char msg[MAX_MSG_LEN]){
     #ifdef DEBUG
         printf("Debug: %s\n", msg);
     #endif
+}
+
+struct Job {
+    int id;
+    int pid;
+    int gpid;
+    char state[MAX_STATE_LEN];
+};
+
+struct Shell {
+    struct Job* jobs[MAX_JOBS];
+    int job_count;
+};
+
+struct Job new_job(int id, int pid, int gpid, const char* state) {
+    struct Job job;
+    job.id = id;
+    job.pid = pid;
+    job.gpid = gpid;
+    strncpy(job.state, state, MAX_STATE_LEN);
+    return job;
+}
+
+void add_job(struct Shell* shell, struct Job job) {
+    if (shell->job_count >= MAX_JOBS) {
+        throw_error("add_job", "Too many jobs\n");
+        return;
+    }
+    shell->jobs[shell->job_count] = malloc(sizeof(struct Job)); // reserve and adds
+    *(shell->jobs[shell->job_count]) = job;
+    shell->job_count++;
+}
+
+void sigint_handler(int signo) {
+    if (foreground_pid > 0) {
+        kill(foreground_pid, SIGINT); // send SIGINT to the foreground process
+    }
 }
 
 void get_input(char* input, size_t size){
@@ -79,10 +104,13 @@ void cd(char* path){
     }
 }
 
-bool check_builtin(char* input){
+bool check_builtin(char* input, struct Shell* shell){
     if (is_same(input, "quit")){ 
         /* No need to do anything main loop will handle 
         but it is a builtin so return true */
+        for (int i = 0; i < shell->job_count; ++i) {
+            free(shell->jobs[i]);
+        }
         return true;
     }else if (is_same(input, "pwd")){
         pwd();
@@ -98,38 +126,46 @@ bool check_builtin(char* input){
     return false; // Not QUIT, PWD or CD
 }
 
-bool check_exe(char* input){
+bool check_exe(char* input, struct Shell* shell){
     char exe_path[MAX_FILENAME_LEN];
-    snprintf(exe_path, sizeof(exe_path), "./%s", input);
+    snprintf(exe_path, sizeof(exe_path), "./%s", input); // adding ./ to path
 
     if (access(exe_path, X_OK) != 0){
-        debug_msg("Not executable!!!");
+        debug_msg("Not executable!!!"); // Not an error can be another command
         return false;
     }
     
     pid_t pid = fork();
+    
     if (pid < 0) {
         throw_error("check_exe", "Fork didn't work");
     }
     else if (pid == 0) { // child
         // Child process: run executable
-        
+        pid_t my_pid = getpid();
+        setpgid(0, 0);  // make new group
         execl(exe_path, input, (char*)NULL);
         throw_error("check_exe", "Error in execl. It returned");
         exit(127);
-    }else {
+    }else { // parent
+        setpgid(pid, pid);
+        struct Job job = new_job(shell->job_count + 1, pid, pid, "Foreground");
+        add_job(shell, job);
+
+        foreground_pid = pid;
         int status;
         waitpid(pid, &status, 0);
+        foreground_pid = -1;
     }
     return true;
 }
 
-void take_action(char* input){
-    if (check_builtin(input)){
+void take_action(char* input, struct Shell* shell){
+    if (check_builtin(input, shell)){
         debug_msg("Is a builtin func");
         return;
     }else {
-        if (check_exe(input)) {
+        if (check_exe(input, shell)) {
             debug_msg("Is a executable file");
             return;
         }
@@ -137,13 +173,14 @@ void take_action(char* input){
 }
 
 int main(){
+    signal(SIGINT, sigint_handler);
+
     char user_input[MAX_INPUT_LEN];
-    struct Job shell;
-    shell.gpid = 10;
-    shell.pid = 10;
+    struct Shell shell;
+    shell.job_count = 0;
     do{
         get_input(user_input, MAX_INPUT_LEN);
-        take_action(user_input);
+        take_action(user_input, &shell);
     }while (!is_same(user_input, "quit"));
     return 0;
 }
--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-#define MAX_ITER 5
+#define MAX_ITER 10
 int main () {
     for (int i = 1; i <= MAX_ITER; ++i){
         printf("Testing %d\n", i);
